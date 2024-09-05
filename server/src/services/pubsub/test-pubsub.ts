1cb42491fd8c64fae5b254a98efd2a7c2c2c4602
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-09-04 22:05:19 -0600

    pub-sub

--- server/src/services/pubsub/test-pubsub.ts
+++ server/src/services/pubsub/test-pubsub.ts
@@ -1,3 +1,8 @@
 import { PubSubService } from "./types";
 
-export class TestPubSub implements PubSubService {}
+export class TestPubSub implements PubSubService {
+  publish: (topic: string, payload: Record<string, unknown>) => Promise<string>;
+  validatePayLoad(payLoad: Record<string, any>): boolean {
+    throw new Error("Method not implemented.");
+  }
+}
